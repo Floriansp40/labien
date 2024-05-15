7a9f0124a2c4efb8c82c0b92a4807e53fe5aac72
Florian <florian@ROGER>
2024-05-15 17:17:13 +0200

    ou pas

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more or la bien
+        Click on the Vite and React logos to learn more or la bien ou pas
       </p>
     </>
   )
